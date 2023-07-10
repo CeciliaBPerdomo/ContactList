1f9cc3fa05f9f05349c658945296104d3c65bbc9
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2023-07-10 23:03:56 +0000

    New Contact

---
+++ src/js/component/formulario.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Formulario = () => {
+	return (
+        <>
+        <div className="container">
+            <div>
+                <h1>Add new Contact</h1>
+            </div>
+        </div>
+        </>
+    )
+};
